ansible/ansible

commit 8dc0276533f2e98d215e752ac09e21b0530b3182

docker: fix type issue (#3491)
* fix type issue

* fix type issue for 'volumes-from'

cloud/docker/docker.py
@@ -1848,12 +1848,12 @@ def main():
             ports           = dict(required=False, default=None, type='list'),
             publish_all_ports = dict(default=False, type='bool'),
             volumes         = dict(default=None, type='list'),
-            volumes_from    = dict(default=None),
+            volumes_from    = dict(default=None, type='list'),
             links           = dict(default=None, type='list'),
             devices         = dict(default=None, type='list'),
-            memory_limit    = dict(default=0),
-            memory_swap     = dict(default=0),
-            cpu_shares      = dict(default=0),
+            memory_limit    = dict(default=0, type='int'),
+            memory_swap     = dict(default=0, type='int'),
+            cpu_shares      = dict(default=0, type='int'),
             docker_url      = dict(),
             use_tls         = dict(default=None, choices=['no', 'encrypt', 'verify']),
             tls_client_cert = dict(required=False, default=None, type='path'),

tgbyte/ansible-modules-core

commit c1b218be8763a439736f4d15e2d8b3d6a9c37f1e

Fix docker memory_limit type regression introduced by 8dc0276533f2e98d215e752ac09e21b0530b3182
The regression prevents Docker containers to be created using human-readable
memory_limits, e.g. 512MB due to a type conversion error:

    argument memory_limit is of type <type 'str'> and we were unable to convert
    to int

This change corrects that issue by reverting to the previous (type-less) module
interface declaration for memory_limit.

cloud/docker/docker.py
@@ -1852,7 +1852,7 @@ def main():
             volumes_from    = dict(default=None, type='list'),
             links           = dict(default=None, type='list'),
             devices         = dict(default=None, type='list'),
-            memory_limit    = dict(default=0, type='int'),
+            memory_limit    = dict(default=0),
             memory_swap     = dict(default=0, type='int'),
             cpu_shares      = dict(default=0, type='int'),
             docker_url      = dict(),